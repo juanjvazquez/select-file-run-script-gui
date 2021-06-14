0a366ba5c41e5c7f3e3a64e986c31894d8084aac
Juan Vazquez <vazquezj@tcd.ie>
2021-06-14 07:24:36 +0100

    dropdown menu added

--- select_and_run.py
+++ select_and_run.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import tkinter.filedialog
+#from tkinter import ttk
 # tkinter.font as tkFont
 #import os
 
@@ -7,23 +9,29 @@ class SelectAndRun(): #Selecting files and running simple scripts on them
         self.master = master
         master.title('Select and Run - Tkinter') 
         
+        self.menu_var = tk.StringVar()
+        self.menu_choices = sorted({'Sum', 'Multiply', 'All'})
+        self.menu_var.set('Sum') # default value
+        self.menu = tk.OptionMenu(master, self.menu_var, *self.menu_choices)
+        
         self.filename = None
         self.browsedvar = tk.StringVar()
         
-        self.label_title = tk.Label(master, text='Title')
+        self.label_title = tk.Label(master, text='Get the Sum/Product of Numbers in a txt file')
         
         vcmd = master.register(self.validate) # we have to wrap the command
         
         self.entry = tk.Entry(master, textvariable = self.browsedvar, validate='key', validatecommand=(vcmd,'%P'))
         #self.printButton = tk.Button(master, text='Print', command=self.printText)
         self.selectFile = tk.Button(master, text='Select File', command=self.browseText)
-        self.runScript = tk.Button(master, text='Run Script on File', command=self.runSumOnFile)
+        self.runScript = tk.Button(master, text='Run', command=lambda:self.chooseScript())
         
         self.label_title.grid(row=0, column=0, columnspan=2)
-        self.entry.grid(row=1, column=0, columnspan=2)
+        self.entry.grid(row=1, column=1, columnspan=1, padx=2, pady=2)
         #self.printButton.grid(row=2, column=0, columnspan=5)
-        self.selectFile.grid(row=3, column=0, columnspan=2)
-        self.runScript.grid(row=4, column=0, columnspan=2)
+        self.menu.grid(row=2, column=1,columnspan=1, padx=2, pady=2)
+        self.selectFile.grid(row=1, column=0, columnspan=1, sticky="ew", padx=2, pady=2)
+        self.runScript.grid(row=2, column=0, columnspan=1, sticky="ew", padx=2, pady=2)
     
     def validate(self, inputText): #This processes input, validates it
         if not inputText:
@@ -41,11 +49,20 @@ class SelectAndRun(): #Selecting files and running simple scripts on them
         self.entry.delete(0, tk.END)
         
     def browseText(self): #Browses for a file, sets file path to self.filename
-        file = tk.filedialog.askopenfilename(initialdir="/", title="Select File",
+        file = tkinter.filedialog.askopenfilename(initialdir="/", title="Select File",
                                           filetypes=(("txt","*.txt"), ("all files", "*.*")))
         self.browsedvar.set(file)
         print(self.filename)
-        
+
+    def chooseScript(self):
+        if self.menu_var.get()=='Sum':
+            print('Sum is: {}'.format(self.runSumOnFile()))
+        if self.menu_var.get()=='Multiply':
+            print('Product is: {}'.format(self.runMultOnFile()))
+        if self.menu_var.get()=='All':
+            print('Sum is: {}'.format(self.runSumOnFile()))
+            print('Product is: {}'.format(self.runMultOnFile()))
+    
     def runSumOnFile(self): #Runs a script (counting) on self.filename
         try:
             directory= str(self.filename)
@@ -56,7 +73,21 @@ class SelectAndRun(): #Selecting files and running simple scripts on them
                 for i in line:
                     if i.isdigit():
                         count=count+int(i)
-            print(count)
+            return count
+        except FileNotFoundError:
+            self.popup()
+    
+    def runMultOnFile(self): #Runs a script (counting) on self.filename
+        try:
+            directory= str(self.filename)
+            file=open(directory,'r')
+            info=file.readlines()
+            prod=1
+            for line in info:
+                for i in line:
+                    if i.isdigit():
+                            prod=prod*int(i)
+            return prod
         except FileNotFoundError:
             self.popup()
             
